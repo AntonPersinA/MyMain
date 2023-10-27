7e7cacc43608cc8586f8fd7521653dbbe3d221f1
antonpersin6@gmail.com <st117333@student.spbu.ru>
2023-10-27 22:01:47 +0300

    New file for big int

---
+++ big_int/big_int.h
@@ -0,0 +1,20 @@
+typedef struct big_int{
+    unsigned char* number;
+    unsigned int length;
+} big_int;
+
+big_int* big_int_get(char *bin_number);
+
+void big_int_print(big_int *number);
+
+void big_int_dlz(big_int *n);
+
+char big_int_equal(big_int *n1, big_int *n2);
+
+void big_int_free(big_int *n);
+
+void big_int_swap(big_int *n1, big_int *n2);
+
+big_int* big_int_copy(big_int *x);
+
+big_int *big_int_sum1(big_int *n1, big_int *n2);
