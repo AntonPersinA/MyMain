02bf75af6f836629035a4f3ac0bb051454c4010e
antonpersin6@gmail.com <st117333@student.spbu.ru>
2023-11-17 01:40:14 +0300

    решил заново все сделать, фурье оно такое

--- big_int_all/main2.c
+++ big_int_all/main2.c
@@ -90,7 +90,8 @@ typedef cd* vcd;
 
 
 #include <mpfr.h>
-#include <math.h>
+#include <stdlib.h>
+#include <tgmath.h>
 
 typedef struct {
     mpfr_t re;
@@ -99,90 +100,53 @@ typedef struct {
 
 typedef complex1* vcomplex1;
 
-
-
-
-vcomplex1 fft1mpfr(const vcomplex1 A, int size)
+vcomplex1 fft1mpfr(const vcomplex1 as, int size, mpfr_prec_t prec) //прек это точность в битах
 {
-    // Initialize PI
-    mpfr_t PI;
-    mpfr_init_set_d(PI, M_PI, MPFR_RNDN);
-
     int n = size;
-    int bits = (int)ceil(log2(n));
-
-    // Reverse bit order
+    int k = 0;
+    while ((1 << k) < n) k++;
     int* rev = (int*)malloc(sizeof(int) * n);
-    for(int i = 0; i < n; ++i) {
-        rev[i] = 0;
-        for(int j = 0; j < bits; ++j)
-            if((i >> j) & 1)
-                rev[i] |= 1 << (bits - 1 - j);
+
+    rev[0] = 0;
+    int high1 = -1;
+    for (int i = 1; i < n; i++) {
+        if ((i & (i - 1)) == 0)
+            high1++;
+        rev[i] = rev[i ^ (1 << high1)];
+        rev[i] |= (1 << (k - high1 - 1));
     }
 
-    // Calculate roots of unity
     vcomplex1 roots = (vcomplex1)malloc(sizeof(complex1) * n);
-    for(int i = 0; i < n; i++) {
-        mpfr_t temp;
-        mpfr_init(roots[i].re);
-        mpfr_init(roots[i].im);
-        mpfr_init(temp);
-
-        mpfr_mul_ui(temp, PI, 2 * i, MPFR_RNDN);
-        mpfr_div_ui(temp, temp, n, MPFR_RNDN);
-        mpfr_mul_si(temp, temp, -1, MPFR_RNDN);
-
-        mpfr_cos(roots[i].re, temp, MPFR_RNDN);
-        mpfr_sin(roots[i].im, temp, MPFR_RNDN);
-        mpfr_clear(temp);
+    for (int i = 0; i < n; i++) {
+        mpfr_t alpha;
+        mpfr_init2(alpha, prec);
+        mpfr_set_d(alpha, 2 * M_PI * i / n, MPFR_RNDN);
+        mpfr_cos(roots[i].re, alpha, MPFR_RNDN);
+        mpfr_sin(roots[i].im, alpha, MPFR_RNDN);
+        mpfr_clear(alpha);
     }
-
 
     vcomplex1 cur = (vcomplex1)malloc(sizeof(complex1) * n);
-    for(int i = 0; i < n; i++){
-        mpfr_init(cur[i].re);
-        mpfr_init(cur[i].im);
+    for (int i = 0; i < n; i++)
+        cur[i] = as[rev[i]];
 
-        mpfr_set(cur[i].re, A[rev[i]].re, MPFR_RNDN);
-        mpfr_set(cur[i].im, A[rev[i]].im, MPFR_RNDN);
-    }
-
-    for(int len = 1; len < n; len <<= 1) {
+    for (int len = 1; len < n; len <<= 1) {
         vcomplex1 ncur = (vcomplex1)malloc(sizeof(complex1) * n);
         int rstep = n / (len * 2);
-        for(int pdest = 0; pdest < n;) {
+        for (int pdest = 0; pdest < n;) {
             int p1 = pdest;
-            for(int i = 0; i < len; i++) {
-                complex1 val, mult;
-                mpfr_t temp_re, temp_im;
+            for (int i = 0; i < len; i++) {
+                complex1 val;
+                mpfr_init2(val.re, prec);
+                mpfr_init2(val.im, prec);
 
-                mpfr_init(val.re);
-                mpfr_init(val.im);
-                mpfr_init(mult.re);
-                mpfr_init(mult.im);
-                mpfr_init(temp_re);
-                mpfr_init(temp_im);
+                mpfr_mul(val.re, roots[i * rstep].re, cur[p1 + len].re, MPFR_RNDN);
+                mpfr_sub(val.re, cur[p1].re, val.re, MPFR_RNDN);
 
-                mpfr_mul(mult.re, roots[i * rstep].re, cur[p1 + len].re, MPFR_RNDN);  // re*re
-                mpfr_mul(temp_im, roots[i * rstep].im, cur[p1 + len].im, MPFR_RNDN);  // im*im
-                mpfr_sub(val.re, mult.re, temp_im, MPFR_RNDN);  // re*re - im*im
+                mpfr_mul(val.im, roots[i * rstep].im, cur[p1 + len].im, MPFR_RNDN);
+                mpfr_add(val.im, cur[p1].im, val.im, MPFR_RNDN);
 
-                mpfr_mul(mult.re, roots[i * rstep].re, cur[p1 + len].im, MPFR_RNDN);  // re*im
-                mpfr_mul(temp_im, roots[i * rstep].im, cur[p1 + len].re, MPFR_RNDN);  // im*re
-                mpfr_add(val.im, mult.re, temp_im, MPFR_RNDN);  // re*im + im*re
-
-                mpfr_add(ncur[pdest].re, cur[p1].re, val.re, MPFR_RNDN);
-                mpfr_sub(ncur[pdest + len].re, cur[p1].re, val.re, MPFR_RNDN);
-                mpfr_add(ncur[pdest].im, cur[p1].im, val.im, MPFR_RNDN);
-                mpfr_sub(ncur[pdest + len].im, cur[p1].im, val.im, MPFR_RNDN);
-
-                mpfr_clear(val.re);
-                mpfr_clear(val.im);
-                mpfr_clear(mult.re);
-                mpfr_clear(mult.im);
-                mpfr_clear(temp_re);
-                mpfr_clear(temp_im);
-
+                ncur[pdest] = val;
                 pdest++, p1++;
             }
             pdest += len;
@@ -191,113 +155,11 @@ vcomplex1 fft1mpfr(const vcomplex1 A, int size)
         cur = ncur;
     }
 
-    mpfr_clear(PI);
-    for(int i = 0; i < n; i++) {
-        mpfr_init(A[i].re);
-        mpfr_init(A[i].im);
-
-        mpfr_clear(roots[i].re);
-        mpfr_clear(roots[i].im);
-    }
-
     free(rev);
     free(roots);
 
     return cur;
 }
-
-
-
-
-void print_complex_array(vcomplex1 cur, int size)
-{
-    printf("Массив комплексных чисел:\n");
-    for(int i = 0; i < size; i++)
-    {
-        char *re_str, *im_str;
-        mpfr_exp_t re_exp, im_exp;
-
-        re_str = mpfr_get_str(NULL, &re_exp, 10, 0, cur[i].re, MPFR_RNDN);
-        im_str = mpfr_get_str(NULL, &im_exp, 10, 0, cur[i].im, MPFR_RNDN);
-
-        printf("Элемент #%d: ", i+1);
-        printf("%se%d + ", re_str, (int)re_exp);
-        printf("%se%d * i\n", im_str, (int)im_exp);
-
-        mpfr_free_str(re_str);
-        mpfr_free_str(im_str);
-    }
-}
-
-
-
-int init_my()
-{
-    int size = 8;  // выберите размер массива
-    vcomplex1 A = (vcomplex1)malloc(sizeof(complex1) * size);
-
-    // Инициализируйте массив A с некоторыми значениями
-    for(int i = 0; i < 4; ++i) {
-        mpfr_init_set_d(A[i].re, (float)(4.0 - i), MPFR_RNDN); // например, устанавливаем вещественную часть равной i+1
-        mpfr_init_set_d(A[i].im, 0.0, MPFR_RNDN); // устанавливаем мнимую часть равной 0
-    }
-
-    for(int i = 4; i < size; ++i) {
-        mpfr_init_set_d(A[i].re, 0.0, MPFR_RNDN); // например, устанавливаем вещественную часть равной i+1
-        mpfr_init_set_d(A[i].im, 0.0, MPFR_RNDN); // устанавливаем мнимую часть равной 0
-    }
-
-    printf("Исходный массив:\n");
-    print_complex_array(A, size);    // печатаем исходный массив
-
-    // Вызываем FFT
-    vcomplex1 result = fft1mpfr(A, size);
-
-    // Печатаем результат FFT
-    printf("\nРезультат FFT:\n");
-    print_complex_array(result, size);
-
-    // Освобождаем память
-    for(int i = 0; i < size; ++i) {
-        mpfr_clear(A[i].re);
-        mpfr_clear(A[i].im);
-        mpfr_clear(result[i].re);
-        mpfr_clear(result[i].im);
-    }
-    free(A);
-    free(result);
-
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
